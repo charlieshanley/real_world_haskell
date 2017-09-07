d23a09902beaa4ed1bfd514b7f1b43a36589d6ff
charlieshanley <charles.scott.hanley+git@gmail.com>
2017-09-06 21:25:25 -0400

    moved json pretty printing stuff to own file, started on barcode scanner.

---
+++ barcode_scanner/Barcode.hs
@@ -0,0 +1,46 @@
+import Data.Array (Array(..), (!), bounds, elems, indices, ixmap, listArray)
+import Control.Applicative ((<$>))
+import Control.Monad (forM_)
+import Data.Char (digitToInt)
+import Data.Ix (Ix(..))
+import Data.List (foldl1', group, sort, sortBy, tails)
+import Data.Maybe (catMaybes, listToMaybe)
+import Data.Ratio (Ratio)
+import Data.Word (Word8)
+import System.Environment (getArgs)
+import qualified Data.ByteString.Lazy.Char8 as L
+import qualified Data.Map as M
+
+
+leftOddList = ["0001101", "0011001", "0010011", "0111101", "0100011",
+               "0110001", "0101111", "0111011", "0110111", "0001011"]
+
+rightList = map complement <$> leftOddList
+    where complement '0' = '1'
+          complement '1' = '0'
+
+leftEvenList = map reverse rightList
+
+parityList = ["111111", "110100", "110010", "110001", "101100",
+              "100110", "100011", "101010", "101001", "100101"]
+
+listToArray :: [a] -> Array Int a
+listToArray xs = listArray (0,l-1) xs
+    where l = length xs
+
+leftOddCodes, leftEvenCodes, rightCodes, parityCodes :: Array Int String
+leftOddCodes = listToArray leftOddList
+leftEvenCodes = listToArray leftEvenList
+rightCodes = listToArray rightList
+parityCodes = listToArray parityList
+
+-- Strict left fold, similar to fold' on lists
+foldA :: Ix k => (a -> b -> a) -> a -> Array k b -> a
+foldA f s a = go s (indices a)
+    where go s (j:js) = let s' = f s (a ! j)
+                        in s' `seq` go s' js
+          go s _ = s
+
+-- Strict left fold using first element of the array as init, similar to foldl1 on lists
+foldA1 :: Ix k => (a -> a -> a) -> Array k a -> a
+foldA1 f a = foldA f (a ! fst (bounds a)) a
